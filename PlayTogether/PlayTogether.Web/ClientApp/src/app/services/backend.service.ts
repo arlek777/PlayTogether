1e0eae483276efa280aafb2350e57675745b3070
Iaroslav Nikishyn <iaroslav.nikishyn@7digital.com>
2018-10-03 17:10:43 +0300

    updated urls

--- PlayTogether/PlayTogether.Web/ClientApp/src/app/services/backend.service.ts
+++ PlayTogether/PlayTogether.Web/ClientApp/src/app/services/backend.service.ts
@@ -5,6 +5,13 @@ import { map } from "rxjs/operators";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 
+enum URLS {
+  login = '/auth/login',
+  updateMainProfileInfo = '/userprofile/updatemaininfo',
+  updateSkillsProfileInfo = '/userprofile/updateskillsinfo',
+  getMainProfileInfo = '/userprofile/getmaininfo',
+  getSkillsProfileInfo = '/userprofile/getskillsinfo'
+};
 
 @Injectable()
 export class BackendService {
@@ -12,6 +19,6 @@ export class BackendService {
   }
 
   login(login: LoginModel): Observable<JwtTokens> {
-    return this.http.post("/auth/login", login).pipe(map(response => response as JwtTokens));
+    return this.http.post(URLS.login, login).pipe(map(response => response as JwtTokens));
   }
 }
