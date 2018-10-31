f0bf67f9ad31a1baa55aca7f6f281d1a3270fec4
Iaroslav Nikishyn <iaroslav.nikishyn@7digital.com>
2018-10-31 17:36:02 +0200

    fixed not logged in error

--- PlayTogether/PlayTogether.Web/ClientApp/src/app/components/new-contact-request-counter/new-contact-request-counter.component.ts
+++ PlayTogether/PlayTogether.Web/ClientApp/src/app/components/new-contact-request-counter/new-contact-request-counter.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ValidationMessages } from '../../constants';
 import { BackendService } from '../../services/backend.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store';
 
 @Component({
   selector: 'new-contact-request-counter',
@@ -11,12 +13,15 @@ export class NewContactRequestCounterComponent {
   public newContactRequestCount = 0;
 
   constructor(
-    private readonly backendService: BackendService) {
+    private readonly backendService: BackendService,
+    private readonly store: Store<AppState>) {
   }
 
   ngOnInit() {
-    this._updateContactRequestCounter();
-    setInterval(() => this._updateContactRequestCounter(), 60000); // update each 1 minute
+    this.store.select(s => s.user.isLoggedIn).subscribe((isLoggedIn) => {
+      this._updateContactRequestCounter();
+      setInterval(() => this._updateContactRequestCounter(), 60000); // update each 1 minute
+    });
   }
 
   private _updateContactRequestCounter() {
